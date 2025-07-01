7abee479acec6700c387421774f5de6bfe82ce57
wwcoderww <wwcoderww@gmail.com>
2025-07-01 15:27:59 -0500

    feat(api): add fakestoreapi data to react-query key 'allItems'

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
+async function fetchData() {
+  try {
+    const data = await fetch("https://fakestoreapi.com/products").then(
+      (res) => {
+        if (!res.ok) throw new Error("Error Loading from API");
+        return res.json();
+      }
+    );
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
 export default function Products() {
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["allItems"],
+    queryFn: fetchData,
+  });
+  console.log(data);
   return <div>Products</div>;
 }
