44a59e55c0beef82cfc0dc35b7be7cd09ddff52d
wwcoderww <wwcoderww@gmail.com>
2025-07-01 14:52:49 -0500

    feat: add navbar with react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    path: "/",
+    children: [
+      { element: <Welcome />, path: "/home" },
+      { element: <Products />, path: "/products" },
+      { element: <Checkout />, path: "/checkout" },
+      { element: <Login />, path: "/login" },
+    ],
+  },
+]);
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <RouterProvider router={router}>
+      <Layout />
+    </RouterProvider>
+  );
 }
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+}
