f77c627f064c04c0b1f8331b78f6496d1c189c9f
wwcoderww <wwcoderww@gmail.com>
2025-08-17 02:18:12 -0500

    style(Product): Items - add loading spinner

--- src/features/products/ProductList.tsx
+++ src/features/products/ProductList.tsx
@@ -4,11 +4,14 @@ import Item from "./Item";
 
 export default function ProductList() {
   const { curSearchItems } = useSearch();
+
   return (
-    <ul className="flex flex-wrap justify-center gap-5 py-6">
-      {curSearchItems.map((item: ProductItemType) => (
-        <Item item={item} key={item.id} />
-      ))}
-    </ul>
+    <div className="flex-1">
+      <ul className="flex flex-wrap justify-center gap-5 py-6">
+        {curSearchItems.map((item: ProductItemType) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
   );
 }
--- src/features/products/components/item/ItemImage.tsx
+++ src/features/products/components/item/ItemImage.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+import LoadingSpinner from "../../../../components/LoadingSpinner";
+
 type ItemImageType = {
   image: string;
 };
 
 export default function ItemImage({ image }: ItemImageType) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
   return (
-    <img
-      src={image}
-      alt="Missing Image"
-      className="block object-fit w-80 h-80 bg-gray-100"
-    />
+    <>
+      {isLoading && (
+        <div className="w-80 h-80">
+          <LoadingSpinner />
+        </div>
+      )}
+      <img
+        src={image}
+        alt="Missing Image"
+        className={`block object-fit w-80 h-80 bg-gray-100 ${
+          isLoading ? `opacity-0` : `opacity-100`
+        }`}
+        onLoad={() => setIsLoading(false)}
+        onError={() => setIsLoading(false)}
+      />
+    </>
   );
 }
